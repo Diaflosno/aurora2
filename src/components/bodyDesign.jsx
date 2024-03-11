e11c2e66af8626a021237c4b46253e05a437f181
Diaflosno <al22304048@utslrc.edu.mx>
2024-03-10 22:51:18 -0700

    Integrando el boton de favorito

--- src/components/bodyDesign.jsx
+++ src/components/bodyDesign.jsx
@@ -1,28 +1,55 @@
 import React from 'react'
 import App from '../App.jsx'
+import { useAuth } from "../firebase/AuthContext";
 
-const ImageCard = ({ image, title, description }) => {
+
+const ImageCard = ({ image, title, description, agregarFavorito }) => {
   const [hover, setHover] = React.useState(false);
+const { user } = useAuth();
+  const uid = user.uid;
+  const AgregarFavorito = () => {
+    // Obtener el usuario actual
+    // Hacer el post a la base de datos de realtime database
+    fetch(`https://aurora-4aa23-default-rtdb.firebaseio.com/favoritos/${uid}.json`, {
+      method: 'POST',
+      body: JSON.stringify({ image, title, description }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Aquí puedes manejar la respuesta del servidor si es necesario
+        console.log('Post exitoso:', data);
+      })
+      .catch(error => {
+        // Aquí puedes manejar el error si ocurre
+        console.error('Error al hacer el post:', error);
+      });
+  };
 
   return (
-    <div >
-    <div
-      className="image-card"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}>
-    <div
-      className={`image-wrapper ${hover ? 'hover' : ''}`}
-      style={{ border: '3px solid black', borderRadius: '10px' }}
-    >
-      <img src={image} alt={title} />
-    </div>
-    </div>
+    <div>
+      <div
+        className="image-card"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div
+          className={`image-wrapper ${hover ? 'hover' : ''}`}
+          style={{ border: '3px solid black', borderRadius: '10px' }}
+        >
+          <img src={image} alt={title} />
+        </div>
+      </div>
       <div className="image-card">
-      <h2 className="tittlecard">{title}</h2>
-      <p className="descriptcard" >{description}</p>
+        <h2 className="tittlecard">{title}</h2>
+        <p className="descriptcard">{description}</p>
+        <button className="btn btn-primary" onClick={AgregarFavorito}>
+          Agregar a favoritos
+        </button>
+      </div>
     </div>
-    </div>
-
   );
 };
 
