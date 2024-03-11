14ccb07426f0936b7a8324b9f92eeb843f5798e7
Jose Moreno <wmorenoglez@gmail.com>
2024-03-11 01:41:52 -0700

    Login en la NavBar y cambios al diseño parte 4

--- src/components/formularioAuth.jsx
+++ src/components/formularioAuth.jsx
@@ -32,7 +32,7 @@ function FormsFirebase( {data} ){
             ) : (
                 <div className="bg-image">
                 <div className="container">
-                <Home />
+                <Home handleGoogle={handleGoogle} />
             </div>
             </div>
             )}
